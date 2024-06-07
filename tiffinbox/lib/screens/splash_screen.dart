6131db60d30a1d4799421f47be72e32b8ee58946
Dhvani Sheth <dhvanisheth25@gmail.com>
2024-06-07 12:35:16 -0400

    added logo to splash screen

--- tiffinbox/lib/screens/splash_screen.dart
+++ tiffinbox/lib/screens/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:tiffinbox/main.dart';
 
@@ -13,7 +12,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
-  late List<Animation<double>> _letterAnimations;
+  late List<Animation<double>> _animations;
 
   final String _text = "TiffinBox";
 
@@ -25,11 +24,12 @@ class _SplashScreenState extends State<SplashScreen>
       duration: const Duration(seconds: 2),
     );
 
-    _letterAnimations = List<Animation<double>>.generate(
-      _text.length,
+    // Create animations for the logo and each letter
+    _animations = List<Animation<double>>.generate(
+      _text.length + 1, // +1 for the logo
       (int index) {
-        final start = index / _text.length;
-        final end = (index + 1) / _text.length;
+        final start = index / (_text.length + 1);
+        final end = (index + 1) / (_text.length + 1);
         return Tween<double>(begin: 0, end: 1).animate(
           CurvedAnimation(
             parent: _animationController,
@@ -69,34 +69,55 @@ class _SplashScreenState extends State<SplashScreen>
             children: <Widget>[
               Row(
                 mainAxisSize: MainAxisSize.min,
-                children: List<Widget>.generate(_text.length, (int index) {
-                  return AnimatedBuilder(
-                    animation: _letterAnimations[index],
+                children: [
+                  // Animated logo
+                  AnimatedBuilder(
+                    animation: _animations[0],
                     builder: (BuildContext context, Widget? child) {
                       return Opacity(
-                        opacity: _letterAnimations[index].value,
-                        child: Text(
-                          _text[index],
-                          style: const TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
-                            color: MyApp.primaryColor,
-                            fontFamily: 'Poppins',
-                            letterSpacing: 1.0,
-                            height: 1.5,
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset(1.0, 1.0),
-                                blurRadius: 3.0,
-                                color: Colors.pink,
-                              ),
-                            ],
-                          ),
+                        opacity: _animations[0].value,
+                        child: Image.asset(
+                          'assets/logo.png',
+                          height: 50.0, // Adjust the height and width as needed
+                          width: 50.0,
                         ),
                       );
                     },
-                  );
-                }),
+                  ),
+                  const SizedBox(width: 10), // Add some spacing between the logo and the text
+                  // Animated text
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: List<Widget>.generate(_text.length, (int index) {
+                      return AnimatedBuilder(
+                        animation: _animations[index + 1], // Adjust index for the text
+                        builder: (BuildContext context, Widget? child) {
+                          return Opacity(
+                            opacity: _animations[index + 1].value,
+                            child: Text(
+                              _text[index],
+                              style: const TextStyle(
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                                color: MyApp.primaryColor,
+                                fontFamily: 'Poppins',
+                                letterSpacing: 1.0,
+                                height: 1.5,
+                                shadows: <Shadow>[
+                                  Shadow(
+                                    offset: Offset(1.0, 1.0),
+                                    blurRadius: 3.0,
+                                    color: Colors.pink,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }),
+                  ),
+                ],
               ),
             ],
           ),
