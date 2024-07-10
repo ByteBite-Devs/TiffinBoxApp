e5452db17ba322375d007a1942ec83eae557230d
heiparth07 <pvd1640@gmail.com>
2024-07-10 14:32:43 -0400

    Added cart screen for usrr

--- tiffinbox/lib/utils/custom_bottom_nav.dart
+++ tiffinbox/lib/utils/custom_bottom_nav.dart
@@ -3,24 +3,27 @@ import 'package:tiffinbox/screens/business/businesshome_screen.dart';
 import 'package:tiffinbox/screens/business/businessprofile_screen.dart';
 import 'package:tiffinbox/screens/home_screen.dart';
 import 'package:tiffinbox/screens/profile_screen.dart';
+import 'package:tiffinbox/screens/cart_screen.dart'; // Import the CartScreen
 import 'package:tiffinbox/main.dart';
-import '../screens/browse_screen.dart'; // For the createSlideRoute function
+import 'package:tiffinbox/utils/color.dart';
+import 'package:tiffinbox/screens/browse_screen.dart'; // For the createSlideRoute function
  
 class CustomBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
- 
   const CustomBottomNavigationBar({
     Key? key,
     required this.currentIndex,
   }) : super(key: key);
- 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: currentIndex,
+      selectedItemColor: primarycolor, // Set the selected item color
+      unselectedItemColor: Colors.grey, // Set the unselected item color
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
         BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Browse'),
+        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'), // Added Cart icon
         BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
       ],
       onTap: (index) {
@@ -37,6 +40,11 @@ class CustomBottomNavigationBar extends StatelessWidget {
             break;
           case 2:
             if (currentIndex != 2) {
+              Navigator.of(context).pushReplacement(createSlideRoute(const CartScreen())); // Navigate to CartScreen
+            }
+            break;
+          case 3:
+            if (currentIndex != 3) {
               Navigator.of(context).pushReplacement(createSlideRoute(const ProfileScreen()));
             }
             break;
@@ -47,7 +55,7 @@ class CustomBottomNavigationBar extends StatelessWidget {
 }
 
 
-class CustomBusinessBottomNavigationBar extends StatelessWidget { 
+class CustomBusinessBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
  
   const CustomBusinessBottomNavigationBar({
@@ -59,6 +67,8 @@ class CustomBusinessBottomNavigationBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: currentIndex,
+      selectedItemColor: primarycolor, // Set the selected item color
+      unselectedItemColor: Colors.grey, // Set the unselected item color
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'BusinessHome'),
         BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Browse'),
